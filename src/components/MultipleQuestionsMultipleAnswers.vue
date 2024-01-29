621d7857bf3b40f4c7f8cd4d665d10d936455b55
vincent.kaiser <vincent.kaiser@sidion.de>
2024-01-29 10:12:40 +0100

    Implementieren von MehrerenFragenMehrereAntworten Typ

---
+++ src/components/MultipleQuestionsMultipleAnswers.vue
@@ -0,0 +1,89 @@
+<script setup>
+    import {defineEmits} from "vue"
+
+    const emit = defineEmits(["submit"])
+
+    const {question} = defineProps(['question'])
+    const emitSubmit = (inputUserAnswers) => {
+        emit("submit", inputUserAnswers)
+    }
+    const inputUserAnswers = []
+</script>
+
+<template>
+    <div class="question-container">
+            <h1 class="question">{{ question.text }}</h1>
+        </div>
+        <div class="options-container">
+            <div 
+            v-for="option in question.options"
+            :key="option.id"
+            class="option"
+            >
+                <div class="option-value">
+                    <p>{{ option.text }}</p>
+                    <input 
+                    class="input-area"
+                    type="text" 
+                    v-model="inputUserAnswers[option.id-1]"
+                    >
+                </div>
+            </div>
+            <button
+            class="submit-button"
+            @click="emitSubmit(inputUserAnswers)"
+            type="button"
+            >Weiter</button>
+        </div>
+</template>
+
+<style scoped>
+.question-container {
+        margin-top: 20px;
+    }
+
+    .question {
+        font-size: 40px;
+        margin-bottom: 20px;
+    }
+
+    .option {
+        display: flex;
+        margin-bottom: 20px;
+        cursor: pointer;
+    }
+    .submit-button {
+        background-color: #0064c0;
+        height: 50px;
+        font-size: 30px;
+        align-items: center;
+        justify-content: center;
+        border-radius: 7.5px;
+    }
+
+    .option-label {
+        background-color: #0064c0;
+        width: 50px;
+        height: 50px;
+        font-size: 30px;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        border-radius: 7.5px;
+    }
+
+    .option-value {
+        background-color: #f0f0f0;
+        width: 100%;
+        font-size: 30px;
+        padding: 0 20px;
+        border-radius: 7.5px;
+        .input-area {
+            background-color: #fff;
+            width: 100%;
+            font-size: 30px;
+            padding: 0 20px;
+            border-radius: 7.5px;
+        }
+    }
+</style>
--- src/views/QuizView.vue
+++ src/views/QuizView.vue
@@ -1,6 +1,7 @@
 <script setup>
     import MultipleChoice from "../components/MultipleChoice.vue"
     import FreeText from "@/components/FreeText.vue"
+    import MultipleQuestionsMultipleAnswers from "@/components/MultipleQuestionsMultipleAnswers.vue"
     import QuizHeader from "../components/QuizHeader.vue"
     import {useRoute} from "vue-router"
     import {ref, watch, computed} from "vue"
@@ -28,14 +29,29 @@
     }
     const onSubmitFreetext = (userAnswer) => {
         const correctAnswer = quiz.questions[currentQuestionIndex.value].optimalAnswer
-        console.log(correctAnswer)
-        console.log(userAnswer)
         if(userAnswer == correctAnswer) {
             numberOfCorrectAnswers.value++
         }
 
         if(quiz.questions.length -1 === currentQuestionIndex.value){
             showResult.value = true
+        }
+
+        currentQuestionIndex.value++
+    }
+    const onSubmitMultipleQuestionsMultipleAnswers = (userAnswer) => {
+        const expectedNumberOfAnswers = quiz.questions[currentQuestionIndex.value].options.length
+        let numberOfCorrectFields = 0
+
+        for(let i = 0; i < userAnswer.length; i++) {
+            console.log(userAnswer[i])
+            if(userAnswer[i] == quiz.questions[currentQuestionIndex.value].options[i].answer) {
+                numberOfCorrectFields++
+            }
+        }
+
+        if(numberOfCorrectFields == expectedNumberOfAnswers) {
+            numberOfCorrectAnswers.value++
         }
 
         currentQuestionIndex.value++
@@ -63,6 +79,11 @@
                 v-else-if="baseCondition() && quiz.questions[currentQuestionIndex].type === 'freeText'"
                 :question="quiz.questions[currentQuestionIndex]"
                 @submit="onSubmitFreetext"
+            />
+            <MultipleQuestionsMultipleAnswers
+                v-else-if="baseCondition() && quiz.questions[currentQuestionIndex].type === 'multipleQuestionsMultipleAnswers'"
+                :question="quiz.questions[currentQuestionIndex]"
+                @submit="onSubmitMultipleQuestionsMultipleAnswers"
             />
             <Result
                 v-else
