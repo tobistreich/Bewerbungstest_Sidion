02df357ca8e9a7af0b3bbf2bd1e85c0810fed7d5
tobistreich <tobias.streich@sidion.de>
2024-01-31 09:05:56 +0100

    Refactor Question.vue -> OneAnswerQuestion.vue
    Deleting irrelevant Files

--- src/views/QuizView.vue
+++ src/views/QuizView.vue
@@ -1,30 +1,34 @@
 <script setup>
-    import Question from "../components/Question.vue"
-    import QuizHeader from "../components/QuizHeader.vue"
-    import {useRoute} from "vue-router"
-    import {ref, watch, computed} from "vue"
-    import questions from "../data/questions.json"
-    import Result from "../components/Result.vue"
-    
-    const quiz = questions.find(q => q.id === 1)
-    const currentQuestionIndex = ref(0)
-    const numberOfCorrectAnswers = ref(0)
-    const showResult = ref(false)
+import OneAnswerQuestion from '../components/OneAnswerQuestion.vue';
+import QuizHeader from '../components/QuizHeader.vue';
+import {useRoute} from 'vue-router';
+import {ref, watch, computed} from 'vue';
+import questions from '../data/questions.json';
+import Result from '../components/Result.vue';
 
-    const questionStatus = computed(() => `${currentQuestionIndex.value}/${quiz.questions.length}`)
+const quiz = questions.find((q) => q.id === 1);
+const currentQuestionIndex = ref(0);
+const numberOfCorrectAnswers = ref(0);
+const showResult = ref(false);
 
-    const barPercentage = computed(() => `${currentQuestionIndex.value/quiz.questions.length * 100}%`)
-    const onOptionSelected = (isCorrect) => {
-        if(isCorrect) {
-            numberOfCorrectAnswers.value++
-        }
+const questionStatus = computed(
+    () => `${currentQuestionIndex.value}/${quiz.questions.length}`,
+);
 
-        if(quiz.questions.length -1 === currentQuestionIndex.value){
-            showResult.value = true
-        }
-
-        currentQuestionIndex.value++
+const barPercentage = computed(
+    () => `${(currentQuestionIndex.value / quiz.questions.length) * 100}%`,
+);
+const onOptionSelected = (isCorrect) => {
+    if (isCorrect) {
+        numberOfCorrectAnswers.value++;
     }
+
+    if (quiz.questions.length - 1 === currentQuestionIndex.value) {
+        showResult.value = true;
+    }
+
+    currentQuestionIndex.value++;
+};
 </script>
 
 <template>
@@ -35,7 +39,7 @@
             id="quiz-header"
         />
         <div>
-            <Question 
+            <OneAnswerQuestion
                 v-if="!showResult"
                 :question="quiz.questions[currentQuestionIndex]"
                 @selectOption="onOptionSelected"
@@ -47,7 +51,6 @@
             />
         </div>
     </div>
-    
 </template>
 
 <style>
